c19ad51e6912ba08b3b73e8543c9c273152f47ef
hero2828 <719857499@qq.com>
2025-07-13 15:58:12 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,51 +1,10 @@
-import { Client, Databases, Users } from "node-appwrite";
-import { Models } from "appwrite";
-
-interface RequestBody extends Models.User<Models.Preferences> {
-  targets: Models.Target[];
-  accessedAt: string;
-}
+import { Client, Databases, Users } from "node-appwrite"
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }: any) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(req.headers["x-appwrite-key"] ?? "");
-  const users = new Users(client);
-  const databases = new Databases(client);
- log('00000')
-  const data: RequestBody = req.bodyJson;
-
-  try {
-    const user = await databases.getDocument(
-      process.env.DB_ID,
-      process.env.COLLECTION_ID,
-      data.$id,
-    );
-
-    if (user) {
-      return res.json({ message: "User already exists" });
-    }
-  } catch (e: any) {
-    log("User does not exist in database. Proceed with creating the user in database.")
-  }
-  
-  try {
-    await databases.createDocument(
-      process.env.DB_ID,
-      process.env.COLLECTION_ID,
-      data.$id,
-      {
-        name: data.name,
-      },
-    );
-    log("User created successfully");
-    return res.json({ message: "User created successfully" });
-  } catch (e: any) {
-    error("Failed to create the user in the database");
-    return res.json({ message: "Failed to create the user in the database" });
-  }
+return res.json({ver:123})
 };
