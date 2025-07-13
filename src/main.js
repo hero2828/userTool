6f5de1ddb438f2c3986e5c1d967123d5b25742a0
hero2828 <719857499@qq.com>
2025-07-13 15:04:22 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
-import { Client, Users } from 'node-appwrite';
+import { Client, Databases, Users } from 'node-appwrite';
 
-// This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(req.headers['x-appwrite-key'] ?? '');
+
   const users = new Users(client);
+  const db = new Databases(client);
 
-  try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
+  const data = req.bodyJson;
+  const user = await db.getDocument(
+    process.env.DB_ID,
+    process.env.COLLECTION_ID,
+    data.$id,
+  );
+  if (user) {
+    return res.json({ msg: '用户已存在' }, 200, {
+      'Access-Control-Allow-Origin': '*',
+    });
   }
-
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
-
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
+  await db.createDocument(
+    process.env.DB_ID,
+    process.env.COLLECTION_ID,
+    data.$id,
+    {
+      name: data.name,
+      create: new Date().toLocaleString(),
+    },
+  );
+  return res.json({ msg: '用户创建成功' }, 200, {
+    'Access-Control-Allow-Origin': '*',
   });
 };
