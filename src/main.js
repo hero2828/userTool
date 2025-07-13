be07216917270e7f1ff356068f07718e0522d744
hero2828 <719857499@qq.com>
2025-07-13 15:44:10 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,35 +1,51 @@
-import { Client, Databases, Users } from 'node-appwrite';
+import { Client, Databases, Users } from "node-appwrite";
+import { Models } from "appwrite";
 
-export default async ({ req, res, log, error }) => {
+interface RequestBody extends Models.User<Models.Preferences> {
+  targets: Models.Target[];
+  accessedAt: string;
+}
+
+// This Appwrite function will be executed every time your function is triggered
+export default async ({ req, res, log, error }: any) => {
+  // You can use the Appwrite SDK to interact with other services
+  // For this example, we're using the Users service
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-
+    .setKey(req.headers["x-appwrite-key"] ?? "");
   const users = new Users(client);
-  const db = new Databases(client);
+  const databases = new Databases(client);
 
-  const data = req.bodyJson;
-  const user = await db.getDocument(
-    process.env.DB_ID,
-    process.env.COLLECTION_ID,
-    data.$id,
-  );
-  if (user) {
-    return res.json({ msg: '用户已存在' }, 200, {
-      'Access-Control-Allow-Origin': '*',
-    });
+  const data: RequestBody = req.bodyJson;
+
+  try {
+    const user = await databases.getDocument(
+      Bun.env["DATABASE_ID"],
+      Bun.env["COLLECTION_ID"],
+      data.$id,
+    );
+
+    if (user) {
+      return res.json({ message: "User already exists" });
+    }
+  } catch (e: any) {
+    log("User does not exist in database. Proceed with creating the user in database.")
   }
-  await db.createDocument(
-    process.env.DB_ID,
-    process.env.COLLECTION_ID,
-    data.$id,
-    {
-      name: data.name,
-      create: new Date().toLocaleString(),
-    },
-  );
-  return res.json({ msg: '用户创建成功' }, 200, {
-    'Access-Control-Allow-Origin': '*',
-  });
+  
+  try {
+    await databases.createDocument(
+      process.env.DB_ID,
+      process.env.COLLECTION_ID,
+      data.$id,
+      {
+        name: data.name,
+      },
+    );
+    log("User created successfully");
+    return res.json({ message: "User created successfully" });
+  } catch (e: any) {
+    error("Failed to create the user in the database");
+    return res.json({ message: "Failed to create the user in the database" });
+  }
 };
